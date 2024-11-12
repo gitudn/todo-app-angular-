384b87976bdc7dcc8a6a75a4de259decead4f789
gitudn <hossain.udn@gmail.com>
2024-11-12 19:05:56 +0100

    Implement addTask function with keydown.enter event binding in all-task.component.ts

--- src/app/components/pages/all-task/all-task.component.ts
+++ src/app/components/pages/all-task/all-task.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpService } from '../../../services/http.service';
 
 @Component({
   selector: 'app-all-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, DatePipe],
   templateUrl: './all-task.component.html',
-  styleUrl: './all-task.component.scss'
+  styleUrl: './all-task.component.scss',
 })
 export class AllTaskComponent {
+  today = new Date();
 
+  newTask = '';
+  taskList: any[] = [];
+
+  httpService = inject(HttpService);
+  ngOnInit() {
+    this.getAllTasks();
+  }
+
+  addTask() {
+    console.log('addTask', this.newTask);
+    this.httpService.addTask(this.newTask).subscribe(() => {
+      this.newTask = '';
+      this.getAllTasks();
+    });
+  }
+
+  getAllTasks() {
+    this.httpService.getAllTasks().subscribe((result: any) => {
+      this.taskList = result;
+    });
+  }
 }
