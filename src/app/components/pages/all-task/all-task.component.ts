cea961cce47ba5a8e66d7bd99f3e03e5fc5d2148
gitudn <hossain.udn@gmail.com>
2024-11-14 21:54:19 +0100

    ngOnInit added , task showing on the frontend

--- src/app/components/pages/all-task/all-task.component.ts
+++ src/app/components/pages/all-task/all-task.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpService } from '../../../services/http.service';
 
 @Component({
   selector: 'app-all-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './all-task.component.html',
-  styleUrl: './all-task.component.scss'
+  styleUrl: './all-task.component.scss',
 })
 export class AllTaskComponent {
+  newTask = '';
+  taskList: any[] = [];
 
+  // Inject HttpService via constructor
+  constructor(private httpService: HttpService) {}
+  // ngOnInit method to load all tasks initially
+  ngOnInit() {
+    this.getAllTasks();
+  }
+
+  addTask() {
+    console.log('addTask', this.newTask);
+    this.httpService.addTask(this.newTask).subscribe(() => {
+      this.newTask = '';
+      this.getAllTasks();
+    });
+  }
+
+  getAllTasks() {
+    this.httpService.getAllTasks().subscribe((result: any) => {
+      this.taskList = result;
+    });
+  }
 }
+
+// export class AllTaskComponent {
+//   newTask = '';
+//   httpService = Inject(HttpService);
+
+//   addTask() {
+//     console.log('addTask', this.newTask);
+//     this.httpService.addTask(this.newTask).subscribe(() => {
+//       console.log('added');
+//     });
+//   }
+// }
---
+++ src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.scss',
+})
+export class TaskListComponent {
+  @Input() taskList: any[] = [];
+}
